14cd006b8bc9a6198a3354bb2a5de41f88ea1932
iam-talhaa <tahakhan4141@gmail.com>
2024-08-21 22:40:11 +0500

    Shared Prefrence

--- lib/ui/homeScreen.dart
+++ lib/ui/homeScreen.dart
@@ -9,14 +9,81 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _heightController = TextEditingController();
+  TextEditingController _weightCOntroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Custom_field(),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              child: Container(
+                height: 300,
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      controller: _nameController,
+                      maxLines: 1,
+                      decoration: InputDecoration(
+                          hintText: "Enter your Name",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: _heightController,
+                      maxLines: 1,
+                      decoration: InputDecoration(
+                          hintText: "Height [inches]",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      controller: _weightCOntroller,
+                      maxLines: 1,
+                      decoration: InputDecoration(
+                          hintText: "Weight [kg]",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.blue),
+                  child: Center(
+                      child: Text(
+                    'Calculate',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white),
+                  )),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
