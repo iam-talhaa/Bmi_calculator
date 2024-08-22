c7610190cf65a4f77bb5311cf8b1dad2a54a9444
iam-talhaa <tahakhan4141@gmail.com>
2024-08-22 22:12:55 +0500

    Bmi Calculator

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print("");
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: PersonList(),
