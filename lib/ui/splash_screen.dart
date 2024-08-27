abc471d84c3d5176de02fc2e4587f8d24ec3a14b
iam-talhaa <tahakhan4141@gmail.com>
2024-08-27 22:25:26 +0500

    updated

--- lib/ui/splash_screen.dart
+++ lib/ui/splash_screen.dart
@@ -24,6 +24,7 @@ class _SplashScreenState extends State<SplashScreen> {
     print('chceh');
     print('chceh');
     print('chceh');
+    print('chceh');
     return Scaffold(
       body: Column(
         children: [
