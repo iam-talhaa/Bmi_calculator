cc8bed07629ffc1297cdfbcd419c22ce0b68f614
iam-talhaa <tahakhan4141@gmail.com>
2024-08-29 23:00:15 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print("");
+    print("");
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
